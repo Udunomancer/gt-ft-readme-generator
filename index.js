6a213d29f564650b1c5d4f699bc9fb4d8eb46fe7
Kyle Chriscoe <kyle717@gmail.com>
2021-01-08 16:01:13 -0500

    required inquirer and fs and confirmed working

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt([
+    {
+        type: "input",
+        question: "Test question",
+        name: "test"
+    }
+]).then(fs.writeFile('test.txt', 'Test', (err) => {
+    err ? console.error(err) : console.log('Test Logged!');
+}));
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -10,4 +22,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+//init();
