8630fda6373281c8dbe67e8cad0f8b033b88b59b
Kyle Chriscoe <kyle717@gmail.com>
2021-01-08 16:42:29 -0500

    linked generateMarkdown to index and abstracted first question

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+
 const fs = require('fs');
+const generator = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
-inquirer.prompt([
-    {
-        type: 'checkbox',
-        name: 'chooseSections',
-        message: 'What sections will you need in your ReadMe? (By Default, all sections are selected)',
-        choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'Contributing', 'Tests'],
-        default: ['Description', 'Table of Contents', 'Installation', 'Usage', 'Contributing', 'Tests']
-    }
-])
+const data = {
+    title: 'Test'
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    generator.generateMarkdown();
+}
 
 // Function call to initialize app
-//init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,37 @@
+const inquirer = require('inquirer');
+
+// First Question, determine what sections to add to ReadMe
+function selectSections() {
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        name: "chooseSections",
+        message:
+          "What sections will you need in your ReadMe? (By Default, all sections are selected)",
+        choices: [
+          "Description",
+          "Table of Contents",
+          "Installation",
+          "Usage",
+          "Contributing",
+          "Tests",
+        ],
+        default: [
+          "Description",
+          "Table of Contents",
+          "Installation",
+          "Usage",
+          "Contributing",
+          "Tests",
+        ],
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+    });
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -11,10 +45,14 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+//function generateMarkdown(data) {
+function generateMarkdown() {
+  selectSections();
+//   return `# ${data.title}
 
-`;
+// `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown
+};
