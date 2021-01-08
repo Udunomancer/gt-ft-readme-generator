80f3f5e0863238e0278313dc7aa6243de75263e6
Kyle Chriscoe <kyle717@gmail.com>
2021-01-08 16:19:53 -0500

    Added initial prompt to select sections to add

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer.prompt([
-    {
-        type: "input",
-        question: "Test question",
-        name: "test"
-    }
-]).then(fs.writeFile('test.txt', 'Test', (err) => {
-    err ? console.error(err) : console.log('Test Logged!');
-}));
-
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer.prompt([
+    {
+        type: 'checkbox',
+        name: 'chooseSections',
+        message: 'What sections will you need in your ReadMe? (By Default, all sections are selected)',
+        choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'Contributing', 'Tests'],
+        default: ['Description', 'Table of Contents', 'Installation', 'Usage', 'Contributing', 'Tests']
+    }
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
