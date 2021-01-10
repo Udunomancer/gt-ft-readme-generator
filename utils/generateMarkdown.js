f906d2d4a167a6948086a52e37d11dcdec4862c5
Kyle Chriscoe <kyle717@gmail.com>
2021-01-09 21:46:49 -0500

    added comments to utils/generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,35 @@
+//Requiring inquirer
 const inquirer = require('inquirer');
 
 async function promptAndReturn() {
-  // if Description > run description
+  //Function that calls section specific functions based on user response to sections to include
+  //Input: n/a
+  //Output: tempData (object of saved user data)
+
+  //Temporary storage of saved user data
   const tempData = {
     listedSections: {}
   };
 
+  //Call function to set Readme/application title
   tempData.title = await setTitle();
 
+  //Call function to have user select sections to include in readme
   questions = await selectSections();
 
+  //If Table of Contents > set it as an included seciton and set initial value
   if(questions.indexOf('Table of Contents') >= 0) {
     tempData.listedSections.tableOfContents = true;
     tempData.contents = '## Table of Contents';
   }
+
+  //If Description > set it as an included section and call function for user to set Description value
   if(questions.indexOf('Description') >= 0) {
     tempData.listedSections.description = true;
     tempData.description = await setDescription();
   }
+
+  //If Installation > set it as an included section and call function for user to set Installation value
   if(questions.indexOf('Installation') >= 0) {
     tempData.listedSections.installation = true;
     tempData.installation = await setInstallation();
@@ -25,6 +37,8 @@ async function promptAndReturn() {
       tempData.contents = tempData.contents + '\n* [Installation](#installation)';
     }
   }
+
+  //If Usage > set it as an included section and call function for user to set Usage value
   if(questions.indexOf('Usage') >= 0) {
     tempData.listedSections.usage = true;
     tempData.usage = await setUsage();
@@ -32,6 +46,8 @@ async function promptAndReturn() {
       tempData.contents = tempData.contents + '\n* [Usage](#usage)';
     }
   }
+
+  //If License > set it as an included section and call function for user to set License values
   if(questions.indexOf('License') >= 0) {
     tempData.listedSections.license = true;
     tempData.license = await setLicense();
@@ -39,6 +55,8 @@ async function promptAndReturn() {
       tempData.contents = tempData.contents + '\n* [License](#license)';
     }
   }
+
+  //If Contributing > set it as an included section and call function for user to set Contributing value
   if(questions.indexOf('Contributing') >= 0) {
     tempData.listedSections.contributing = true;
     tempData.contribution = await setContribution();
@@ -46,6 +64,8 @@ async function promptAndReturn() {
       tempData.contents = tempData.contents + '\n* [Contributing](#contributing)';
     }
   }
+
+  //If Tests > set it as an included section and call function for user to set Tests value
   if(questions.indexOf('Tests') >= 0) {
     tempData.listedSections.tests = true;
     tempData.tests = await setTests();
@@ -53,6 +73,8 @@ async function promptAndReturn() {
       tempData.contents = tempData.contents + '\n* [Tests](#tests)';
     }
   }
+
+  //If Questions > set it as an included section and call function for user to set Questions values
   if(questions.indexOf('Questions') >= 0) {
     tempData.listedSections.questions = true;
     tempData.questions = await setQuestion();
@@ -61,11 +83,15 @@ async function promptAndReturn() {
     }
   }
 
+  //Return user responses as an object
   return tempData;
 }
 
-// Set the title of the application
 async function setTitle() {
+  //Function to set the title of the application based on user input
+  //Input: n/a
+  //Output: Promise with saved title from user input
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -81,6 +107,10 @@ async function setTitle() {
 }
 
 async function setDescription() {
+  //Function to set the description section based on user input
+  //Input: n/a
+  //Output: Promise with saved description from user input
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -96,6 +126,10 @@ async function setDescription() {
 }
 
 async function setInstallation() {
+  //Function to set the installation section based on user input
+  //Input: n/a
+  //Output: Promise with saved installation value from user input
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -111,6 +145,10 @@ async function setInstallation() {
 }
 
 async function setUsage() {
+  //Function to set the usage section based on user input
+  //Input: n/a
+  //Output: Promise with saved usage value from user input
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -126,6 +164,10 @@ async function setUsage() {
 }
 
 async function setLicense() {
+  //Function to set the license values based on user input
+  //Input: n/a
+  //Output: Promise with object of badge and link values
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -164,6 +206,10 @@ async function setLicense() {
 }
 
 async function setContribution() {
+  //Function to set the contribution section based on user input
+  //Input: n/a
+  //Output: Promise with contribution section value
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -179,6 +225,10 @@ async function setContribution() {
 }
 
 async function setTests() {
+  //Function to set the tests section based on user input
+  //Input: n/a
+  //Output: Promise with tests value from user
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -194,6 +244,10 @@ async function setTests() {
 }
 
 async function setQuestion() {
+  //Function to save email and GitHub profile based on user input
+  //Input: n/a
+  //output: Promise with questions section values
+
   return new Promise(function(resolve, reject) {
     inquirer.prompt([
       {
@@ -213,8 +267,11 @@ async function setQuestion() {
   })
 }
 
-// determine what sections to add to ReadMe
 async function selectSections() {
+  //Function to select which sections to include in the ReadMe based on user input
+  //Input: n/a
+  //Output: Promise with an array strings that list the sections to include
+
   return new Promise(function(resolve, reject) {
     inquirer
       .prompt([
@@ -251,93 +308,70 @@ async function selectSections() {
     });
 }
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  // selectSections().then((response) => {
-  //   return response;
-  // });
+  //Function to generate the text to include in the Readme file
+  //Input: Object of saved data from user input
+  //Output: String of completed content to include in the Readme
+
+  //Add Title to string
   let content = `# ${data.title}`;
 
+  //Add license badge to string, IF included
   if(data.listedSections.license) {
     content += '\n';
     content += data.license.badge; 
   }
 
+  //Add description to string, IF included
   if(data.listedSections.description) {
     content += '\n\n## Description\n\n';
     content += data.description;
   }
 
+  //Add table of contents to string, IF included
   if(data.listedSections.tableOfContents) {
     content += '\n\n';
     content += data.contents;
   }
 
+  //Add installation to string, IF included
   if(data.listedSections.installation) {
     content += '\n\n## <a name="installation"></a> Installation\n\n';
     content += data.installation;
   }
 
+  //Add usage to string, IF included
   if(data.listedSections.usage) {
     content += '\n\n## <a name="usage"></a> Usage\n\n';
     content += data.usage;
   }
 
+  //Add license to string, IF included
   if(data.listedSections.license) {
     content += '\n\n## <a name="license"></a> License\n\n';
     content += data.license.link;
   }
 
+  //Add contributing section to string, IF included
   if(data.listedSections.contributing) {
     content += '\n\n## <a name="contributing"></a> Contributing\n\n';
     content += data.contribution;
   }
 
+  //Add tests section to string, IF included
   if(data.listedSections.tests) {
     content += '\n\n## <a name="tests"></a> Tests\n\n';
     content += data.tests;
   }
 
+  //Add questions section to string, IF included
   if(data.listedSections.questions) {
     content += '\n\n## <a name="questions"></a> Questions\n\nHave questions?\n';
     content += data.questions;
   }
 
+  //Return the completed string
   return content;
-
-
-  
-//   return `# ${data.title}
-
-// ${data.description}
-
-// ${data.contents}
-
-// ${data.installation}
-
-// ${data.usage}
-
-// ## <a name="license"></a> License
-
-// ${data.license.link}
-
-// ${data.contribution}
-
-// ${data.tests}
-
-// ${data.questions}`;
 }
 
 module.exports = {
